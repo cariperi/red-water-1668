979f31395c0565f0fff4484aa73b8e22dd61f6a6
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 08:37:03 -0400

    feat: add seeds data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dish.destroy_all
+Ingredient.destroy_all
+IngredientDish.destroy_all
+Chef.destroy_all
+
+@chef_1 = Chef.create!(name: "Chef 1")
+@chef_2 = Chef.create!(name: "Chef 2")
+
+@dish_1 = @chef_1.dishes.create!(name: "Dish 1", description: "ABC")
+@dish_2 = @chef_1.dishes.create!(name: "Dish 2", description: "DEF")
+
+@dish_3 = @chef_2.dishes.create!(name: "Dish 3", description: "GHI")
+@dish_4 = @chef_2.dishes.create!(name: "Dish 4", description: "XYZ")
+
+@ingredient_1 = Ingredient.create!(name: "Sugar", calories: 10)
+@ingredient_2 = Ingredient.create!(name: "Salt", calories: 20)
+@ingredient_3 = Ingredient.create!(name: "Pepper", calories: 30)
+
+IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_1.id)
+IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_3.id)
+IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_2.id)
+IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_1.id)
+IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_4.id)
