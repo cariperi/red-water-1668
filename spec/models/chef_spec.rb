b8251bdb4248b181dc67aa1cf5b53078285f83a0
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 10:33:43 -0400

    test: add test for most popular 3 ingredients method to chef model

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -38,5 +38,23 @@ RSpec.describe Chef, type: :model do
         expect(@chef_2.get_ingredient_names).to eq("Pepper and Sugar")
       end
     end
+
+    describe '#most_popular_3_ingredients' do
+      it 'returns a list of the 3 most popular ingredient names for this chef, sorted in order most to least' do
+        @ingredient_4 = Ingredient.create!(name: "Apples", calories: 10)
+        @dish_5 = @chef_1.dishes.create(name: "Dish 5", description: "123")
+        @dish_6 = @chef_1.dishes.create(name: "Dish 6", description: "456")
+
+        IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_5.id)
+        IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_6.id)
+        IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_5.id)
+        IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_6.id)
+        IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_5.id)
+        IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_6.id)
+        IngredientDish.create!(ingredient_id: @ingredient_4.id, dish_id: @dish_5.id)
+
+        expect(@chef_1.most_popular_3_ingredients).to eq([@ingredient_2.name, @ingredient_1.name, @ingredient_3.name])
+      end
+    end
   end
 end
