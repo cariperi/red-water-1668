e43f932493cb51aedd9bf6c5d7b17616084c0a08
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 09:34:26 -0400

    test: add test for add ingredient form to dish show page - user story 2

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -23,35 +23,66 @@ RSpec.describe 'the dish show page' do
     IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_4.id)
   end
 
-  it 'should display the name, description, and ingredients for the dish' do
-    visit dish_path(@dish_1)
+  describe "user story 1" do
+    it 'should display the name, description, and ingredients for the dish' do
+      visit dish_path(@dish_1)
 
-    expect(page).to have_content("Dish Name: #{@dish_1.name}")
-    expect(page).to have_content("Description: #{@dish_1.description}")
+      expect(page).to have_content("Dish Name: #{@dish_1.name}")
+      expect(page).to have_content("Description: #{@dish_1.description}")
 
-    expect(page).to have_content("Ingredients for this Dish:")
+      expect(page).to have_content("Ingredients for this Dish:")
 
-    within("#ingredient-#{@ingredient_1.id}") do
+      within("#ingredient-#{@ingredient_1.id}") do
+        expect(page).to have_content("Ingredient Name: #{@ingredient_1.name}")
+      end
+
+      within("#ingredient-#{@ingredient_2.id}") do
+        expect(page).to have_content("Ingredient Name: #{@ingredient_2.name}")
+      end
+
+      expect(page).to_not have_content("Ingredient Name: #{@ingredient_3.name}")
+    end
+
+    it 'should display a total calorie count for the dish' do
+      visit dish_path(@dish_1)
+
+      expect(page).to have_content("Total Calories: 30")
+    end
+
+    it 'should display the chefs name' do
+      visit dish_path(@dish_1)
+
+      expect(page).to have_content("Chef Name: #{@chef_1.name}")
+      expect(page).to_not have_content("Chef Name: #{@chef_2.name}")
+    end
+  end
+
+  describe "user story 2" do
+    it 'displays a form to add an existing ingredient to the specific dish' do
+      visit dish_path(@dish_1)
+
+      within("#add-ingredient") do
+        expect(page).to have_content("Add an Ingredient")
+        expect(page).to have_field(:ingredient_id)
+      end
+
       expect(page).to have_content("Ingredient Name: #{@ingredient_1.name}")
-    end
-
-    within("#ingredient-#{@ingredient_2.id}") do
       expect(page).to have_content("Ingredient Name: #{@ingredient_2.name}")
+      expect(page).to_not have_content("Ingredient Name: #{@ingredient_3.name}")
     end
 
-    expect(page).to_not have_content("#{@ingredient_3.name}")
-  end
+    it 'when I add an ingredient ID and click submit it adds the ingredient to the dish and returns to show page' do
+      visit dish_path(@dish_1)
 
-  it 'should display a total calorie count for the dish' do
-    visit dish_path(@dish_1)
+      within("#add-ingredient") do
+        fill_in :ingredient_id, with: @ingredient_3.id
+        click_button("Submit")
+      end
 
-    expect(page).to have_content("Total Calories: 30")
-  end
-
-  it 'should display the chefs name' do
-    visit dish_path(@dish_1)
-
-    expect(page).to have_content("Chef Name: #{@chef_1.name}")
-    expect(page).to_not have_content("Chef Name: #{@chef_2.name}")
+      expect(current_path).to eq(dish_path(@dish_1))
+      expect(page).to have_content("Ingredient Name: #{@ingredient_1.name}")
+      expect(page).to have_content("Ingredient Name: #{@ingredient_2.name}")
+      expect(page).to have_content("Ingredient Name: #{@ingredient_3.name}")
+    end
   end
 end
