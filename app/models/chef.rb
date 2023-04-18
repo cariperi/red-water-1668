ef65a74bdfb3a103dc279a3bf6d38e40d12a1d25
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 10:10:43 -0400

    feat: add get ingredient names instance method to chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+
+  def get_ingredient_names
+    ingredients.order(:name).distinct.pluck(:name).to_sentence
+  end
 end
