a39205f8622d26cfd6def23b6c0d33fcd3ddd665
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 10:11:13 -0400

    test: add tests for chef show page link to chef ingredients index (user story 3)

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'the chef show page' do
+  before(:each) do
+    @chef_1 = Chef.create!(name: "Chef 1")
+    @chef_2 = Chef.create!(name: "Chef 2")
+
+    @dish_1 = @chef_1.dishes.create!(name: "Dish 1", description: "ABC")
+    @dish_2 = @chef_1.dishes.create!(name: "Dish 2", description: "DEF")
+
+    @dish_3 = @chef_2.dishes.create!(name: "Dish 3", description: "GHI")
+    @dish_4 = @chef_2.dishes.create!(name: "Dish 4", description: "XYZ")
+
+    @ingredient_1 = Ingredient.create!(name: "Sugar", calories: 10)
+    @ingredient_2 = Ingredient.create!(name: "Salt", calories: 20)
+    @ingredient_3 = Ingredient.create!(name: "Pepper", calories: 50)
+
+    IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_1.id)
+    IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_3.id)
+    IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_2.id)
+    IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_1.id)
+    IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_4.id)
+  end
+
+  describe "user story 3" do
+    it 'shows a link to a chef_ingredients index page' do
+      visit chef_path(@chef_1)
+
+      expect(page).to have_content("Chef Show Page for Chef 1")
+      expect(page).to have_link("View All Ingredients")
+    end
+
+    it 'the link goes to a chefs ingredients index page with a unique list of the names of all ingredients used by the chef' do
+      visit chef_path(@chef_1)
+      click_link "View All Ingredients"
+
+      expect(current_path).to eq(chef_ingredients_path(@chef_1))
+      expect(page).to have_content("Chef Name: #{@chef_1.name}")
+      expect(page).to have_content("Chef's Ingredients: #{@ingredient_2.name} and #{@ingredient_1.name}")
+      expect(page).to_not have_content("#{@ingredient_3.name}")
+    end
+  end
+end
