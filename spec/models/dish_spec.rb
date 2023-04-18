ea618fd3185db754c67b0f2ff304a4c005fff530
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-18 09:05:01 -0400

    test: add test for get calorie count instance method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe Dish, type: :model do
     it {should have_many :ingredient_dishes}
     it {should have_many(:ingredients).through(:ingredient_dishes)}
   end
+
+  describe "instance methods" do
+    before(:each) do
+      @chef_1 = Chef.create!(name: "Chef 1")
+      @chef_2 = Chef.create!(name: "Chef 2")
+
+      @dish_1 = @chef_1.dishes.create!(name: "Dish 1", description: "ABC")
+      @dish_2 = @chef_1.dishes.create!(name: "Dish 2", description: "DEF")
+
+      @dish_3 = @chef_2.dishes.create!(name: "Dish 3", description: "GHI")
+      @dish_4 = @chef_2.dishes.create!(name: "Dish 4", description: "XYZ")
+
+      @ingredient_1 = Ingredient.create!(name: "Sugar", calories: 10)
+      @ingredient_2 = Ingredient.create!(name: "Salt", calories: 20)
+      @ingredient_3 = Ingredient.create!(name: "Pepper", calories: 50)
+
+      IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_1.id)
+      IngredientDish.create!(ingredient_id: @ingredient_1.id, dish_id: @dish_3.id)
+      IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_2.id)
+      IngredientDish.create!(ingredient_id: @ingredient_2.id, dish_id: @dish_1.id)
+      IngredientDish.create!(ingredient_id: @ingredient_3.id, dish_id: @dish_4.id)
+    end
+
+    describe "#get_calorie_count" do
+      it 'returns the calorie count for a specific dish' do
+        expect(@dish_1.get_calorie_count).to eq(30)
+        expect(@dish_2.get_calorie_count).to eq(20)
+      end
+    end
+  end
 end
